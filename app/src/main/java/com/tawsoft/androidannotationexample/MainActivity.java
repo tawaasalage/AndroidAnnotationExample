c20bf244b383c5614a40cd6b3f0fbc044b5c80d4
tawaasalage <thilina@osmium.com.au>
2015-04-09 13:23:31 +0530

    Initial commit

--- app/src/main/java/com/tawsoft/androidannotationexample/MainActivity.java
+++ app/src/main/java/com/tawsoft/androidannotationexample/MainActivity.java
@@ -1,19 +1,29 @@
 package com.tawsoft.androidannotationexample;
 
-import android.support.v7.app.ActionBarActivity;
+
 import android.os.Bundle;
-import org.androidannotations.annotations.EActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class MainActivity extends ActionBarActivity
+{
+
+    @InjectView(R.id.myInput)EditText myInput;
+    @InjectView(R.id.myButton)Button myButton;
+    @InjectView(R.id.myTextView)TextView myTextView;
 
 
-@EActivity(R.layout.activity_main)
-public class MainActivity extends ActionBarActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
     }
 
 
